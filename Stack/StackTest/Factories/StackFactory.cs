856fcfd220e5a8b6219dc6079e3d2faa48b379b9
Angello Astorga <angello.astorga@gmail.com>
2019-02-28 22:05:33 -0600

    debugging pex and learner configurations

--- Stack/StackTest/Factories/StackFactory.cs
+++ Stack/StackTest/Factories/StackFactory.cs
@@ -10,10 +10,9 @@ namespace Stack.Test.Factories
     public static class StackFactory
     {
         [PexFactoryMethod(typeof(Stack.Stack<int>))]
-        public static Stack.Stack<int> Create(int[] elems)
+        public static Stack.Stack<int> Create([PexAssumeNotNull]int[] elems)
         {
             
-            PexAssume.IsTrue(elems != null && elems.Length < 11);
             PexAssume.TrueForAll(0, elems.Length, _i => elems[_i] > -11 && elems[_i] < 11);
             Stack.Stack<int> ret = new Stack.Stack<int>(elems.Length+2 );// DataStructure has big enough capacity for Commutativity Test
             for (int i = 0; i < elems.Length; i++)
--- Stack/StackTest/StackContractTest.cs
+++ Stack/StackTest/StackContractTest.cs
@@ -13,23 +13,33 @@ namespace Stack.Test
     [PexClass,TestClass]
     public partial class StackContractTest
     {
+        /* Post Condition with entropy:
+         * ((New_s1ContainsX) && (New_Top == Old_x) && (New_s1Count == 1 + Old_s1Count) && (New_Top == New_x) && (Old_s1ContainsX)) || 
+         * ((New_s1ContainsX) && (New_Top == Old_x) && (New_s1Count == 1 + Old_s1Count) && (New_Top == New_x) && ( ! (Old_Top == New_Top)))
+         * 
+         * 
+         * Post Condition with max conjuncts:
+         * 
+         * 
+         */
+
         //[PexMethod(TestEmissionFilter= PexTestEmissionFilter.All)]
         [PexMethod]
         public void PUT_PushContract([PexAssumeUnderTest]Stack.Stack<int> s1, int x)
         {
-            
-            //PexAssume.IsTrue(s1.Count > 0);
 
+            PexAssume.IsTrue(s1.Count > 0);
+            PexAssume.IsTrue(x < 11 && x > -11);
 
             int Old_s1Count = s1.Count;
+
+
             int Old_Top = s1.Peek();
             int Old_x = x;
             bool Old_s1ContainsX = s1.Contains(x);
 
-            //PexAssume.IsTrue(Old_x != Old_Top); 
-
             AssumePrecondition.IsTrue(true);
-            
+
             s1.Push(x);
 
             int New_s1Count = s1.Count;
@@ -46,7 +56,7 @@ namespace Stack.Test
             PexObserve.ValueForViewing("$old_s1_contains_x", Old_s1ContainsX);
             PexObserve.ValueForViewing("$new_s1_contains_x", New_s1ContainsX);
 
-            PexAssert.IsTrue(((New_s1ContainsX) && (New_x == Old_x) && (New_s1Count == 1 + Old_s1Count) && (New_Top == New_x) && (Old_s1Count >= 1) && (Old_Top >= -10)));
+            PexAssert.IsTrue(((New_s1ContainsX) && (New_x == Old_x) && (New_s1Count == 1 + Old_s1Count) && (New_Top == New_x) && (Old_s1ContainsX)) || ((New_s1ContainsX) && (New_x == Old_x) && (New_s1Count == 1 + Old_s1Count) && (New_Top == New_x) && ( ! (Old_Top == New_x))));
         }
 
 
@@ -56,24 +66,15 @@ namespace Stack.Test
 
             PexAssume.IsTrue(s1.Count > 0);
 
-
-
             //intVariables = ['Old_s1Count', 'New_s1Count','Old_Top','New_Top', 'Old_x','New_x']
             //boolVariables = ["New_s1ContainsX", "Old_s1ContainsX"]
 
             int Old_s1Count = s1.Count;
-            //int Old_Top = s1.Peek();
-            //int Old_x = x;
-            //bool Old_s1ContainsX = s1.Contains(x);
-
-            //PexAssume.IsTrue(Old_x != Old_Top); 
-
-            AssumePrecondition.IsTrue(true);
-            //PexObserve.ValueForViewing("$input_Old_s1Count", s1.Count);
-            //PexObserve.ValueForViewing("$input_Old_s1Peek", s1.Peek());
-            //PexObserve.ValueForViewing("$input_x", x);
-
+            int Old_ret = -12;
+            AssumePrecondition.IsTrue(s1.Count > 0);
+            
             int New_ret = s1.Pop();
+            Old_ret = New_ret;
 
             int New_s1Count = s1.Count;
             //int New_Top = s1.Peek();
@@ -82,12 +83,13 @@ namespace Stack.Test
 
             PexObserve.ValueForViewing("$old_s1_Count", Old_s1Count);
             PexObserve.ValueForViewing("$new_s1_Count", New_s1Count);
+            PexObserve.ValueForViewing("$Old_ret", Old_ret);
             PexObserve.ValueForViewing("$new_ret", New_ret);
             //PexObserve.ValueForViewing("$old_s1_Peek", Old_Top);
             //PexObserve.ValueForViewing("$new_s1_Peek", New_Top);
 
 
-            PexAssert.IsTrue(((Old_s1Count == 1) && (New_s1Count == 0) && (New_ret == 10)));
+            PexAssert.IsTrue(((Old_s1Count == New_ret) && ( ! (New_s1Count == Old_s1Count)) && (Old_s1Count == Old_ret)) || (( ! (Old_s1Count == Old_ret)) && ( ! (Old_s1Count == New_ret))));
 
         }
 
@@ -98,37 +100,25 @@ namespace Stack.Test
             PexAssume.IsTrue(s1.Count > 0);
 
 
-
-            //intVariables = ['Old_s1Count', 'New_s1Count','Old_Top','New_Top', 'Old_x','New_x']
-            //boolVariables = ["New_s1ContainsX", "Old_s1ContainsX"]
-
             int Old_s1Count = s1.Count;
-            //int Old_Top = s1.Peek();
-            //int Old_x = x;
-            //bool Old_s1ContainsX = s1.Contains(x);
-
-            //PexAssume.IsTrue(Old_x != Old_Top); 
-
+            int Old_ret = -12 ;
+            
             AssumePrecondition.IsTrue(true);
-            //PexObserve.ValueForViewing("$input_Old_s1Count", s1.Count);
-            //PexObserve.ValueForViewing("$input_Old_s1Peek", s1.Peek());
-            //PexObserve.ValueForViewing("$input_x", x);
-
+            
             int New_ret = s1.Peek();
 
             int New_s1Count = s1.Count;
-            //int New_Top = s1.Peek();
-            //int New_x = x;
-            //bool New_s1ContainsX = s1.Contains(x);
+            Old_ret = New_ret;
 
             PexObserve.ValueForViewing("$old_s1_Count", Old_s1Count);
             PexObserve.ValueForViewing("$new_s1_Count", New_s1Count);
+            PexObserve.ValueForViewing("$old_s1_ret", Old_ret);
             PexObserve.ValueForViewing("$new_ret", New_ret);
             //PexObserve.ValueForViewing("$old_s1_Peek", Old_Top);
             //PexObserve.ValueForViewing("$new_s1_Peek", New_Top);
 
 
-            PexAssert.IsTrue(((Old_s1Count == 1) && (New_s1Count == 1) && (New_ret == 10)));
+            PexAssert.IsTrue(((Old_s1Count == New_ret) && (New_s1Count == New_ret) && (New_ret == Old_ret)) || (( ! (Old_s1Count == Old_ret)) && ( ! (New_s1Count == Old_ret)) && ( ! (New_s1Count == New_ret)) && ( ! (Old_s1Count == New_ret))));
 
         }
 
