a47b27c8d6e8d42577ac444a8bff54752a79dd01
wwwsssyyy <31974413+wwwsssyyy@users.noreply.github.com>
2019-03-17 02:38:36 -0500

    add HashSet

---
+++ HashSet/HashSet/Utility/HashSetEqualityComparer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace HashSet.Utility
+{
+    /// <summary>
+    /// Equality comparer for hashsets of hashsets
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class HashSetEqualityComparer<T> : IEqualityComparer<HashSet<T>>
+    {
+        private readonly IEqualityComparer<T> m_comparer;
+
+        public HashSetEqualityComparer()
+        {
+            m_comparer = EqualityComparer<T>.Default;
+        }
+
+        // using m_comparer to keep equals properties in tact; don't want to choose one of the comparers
+        public bool Equals(HashSet<T> x, HashSet<T> y)
+        {
+            // return HashSet<T>.HashSetEquals(x, y, m_comparer);
+            return x.SetEquals(y);
+        
+        }
+
+        public int GetHashCode(HashSet<T> obj)
+        {
+            int hashCode = 0;
+            if (obj != null)
+            {
+                foreach (T t in obj)
+                {
+                    hashCode = hashCode ^ (m_comparer.GetHashCode(t) & 0x7FFFFFFF);
+                }
+            } // else returns hashcode of 0 for null hashsets
+            return hashCode;
+        }
+
+        // Equals method for the comparer itself. 
+        public override bool Equals(object obj)
+        {
+            HashSetEqualityComparer<T> comparer = obj as HashSetEqualityComparer<T>;
+            if (comparer == null)
+            {
+                return false;
+            }
+            return (m_comparer == comparer.m_comparer);
+        }
+
+        public override int GetHashCode()
+        {
+            return m_comparer.GetHashCode();
+        }
+    }
+}
---
+++ HashSet/HashSetTest/HashSetContractTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Text;
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Settings;
+using Microsoft.Pex.Framework.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HashSet;
+using PexAPIWrapper;
+
+namespace HashSet.Test
+{
+    [PexClass, TestClass]
+    public partial class HashSetContractTest
+    {
+        [PexMethod]
+        public void PUT_AddContract([PexAssumeUnderTest]HashSet.HashSet<int> hs, int x)
+        {
+            AssumePrecondition.IsTrue(true);
+
+            int Old_hsCount = hs.Count;
+            int Old_x = x;
+            bool Old_Ret = PexChoose.Value<bool>("default_Ret"); ;
+            bool Old_hsContainsX = hs.Contains(x);
+
+            bool New_Ret = hs.Add(x);
+
+            int New_hsCount = hs.Count;
+            int New_x = x;
+            bool New_hsContainsX = hs.Contains(x);
+
+            PexObserve.ValueForViewing("$old_hs_Count", Old_hsCount);
+            PexObserve.ValueForViewing("$new_hs_Count", New_hsCount);
+            PexObserve.ValueForViewing("$old_hs_x", Old_x);
+            PexObserve.ValueForViewing("$new_hs_x", New_x);
+            PexObserve.ValueForViewing("$old_hs_contains_x", Old_hsContainsX);
+            PexObserve.ValueForViewing("$new_hs_contains_x", New_hsContainsX);
+            PexObserve.ValueForViewing("$Old_ret", Old_Ret);
+            PexObserve.ValueForViewing("$New_ret", New_Ret);
+
+            PexAssert.IsTrue(((New_hsContainsX) && (!Old_hsContainsX) && (New_Ret) && (New_x == Old_x) && (New_hsCount == 1 + Old_hsCount)) || ((New_hsContainsX) && (Old_hsContainsX) && (!New_Ret) && (New_x == Old_x) && (New_hsCount == Old_hsCount)));
+        }
+
+        [PexMethod]
+        public void PUT_RemoveContract([PexAssumeUnderTest]HashSet.HashSet<int> hs, int x)
+        {
+            AssumePrecondition.IsTrue(hs.Count > 0);
+
+            int Old_hsCount = hs.Count;
+            int Old_x = x;
+            bool Old_Ret = PexChoose.Value<bool>("default_Ret"); ;
+            bool Old_hsContainsX = hs.Contains(x);
+
+            bool New_Ret = hs.Remove(x);
+
+            int New_hsCount = hs.Count;
+            int New_x = x;
+            bool New_hsContainsX = hs.Contains(x);
+
+            PexObserve.ValueForViewing("$old_hs_Count", Old_hsCount);
+            PexObserve.ValueForViewing("$new_hs_Count", New_hsCount);
+            PexObserve.ValueForViewing("$old_hs_x", Old_x);
+            PexObserve.ValueForViewing("$new_hs_x", New_x);
+            PexObserve.ValueForViewing("$old_hs_contains_x", Old_hsContainsX);
+            PexObserve.ValueForViewing("$new_hs_contains_x", New_hsContainsX);
+            PexObserve.ValueForViewing("$Old_ret", Old_Ret);
+            PexObserve.ValueForViewing("$New_ret", New_Ret);
+
+            PexAssert.IsTrue(((!New_hsContainsX) && (!Old_hsContainsX) && (!New_Ret) && (New_x == Old_x) && (New_hsCount == Old_hsCount)) || ((!New_hsContainsX) && (Old_hsContainsX) && (New_Ret) && (New_x == Old_x) && (New_hsCount == Old_hsCount - 1)));
+        }
+
+        [PexMethod]
+        public void PUT_CountContract([PexAssumeUnderTest]HashSet.HashSet<int> hs)
+        {
+            AssumePrecondition.IsTrue(true);
+
+            int Old_hsCount = hs.Count;
+            int Old_Ret = 2;
+
+            int New_Ret = hs.Count;
+
+            int New_hsCount = hs.Count;
+
+            PexObserve.ValueForViewing("$old_hs_Count", Old_hsCount);
+            PexObserve.ValueForViewing("$new_hs_Count", New_hsCount);
+            PexObserve.ValueForViewing("$Old_ret", Old_Ret);
+            PexObserve.ValueForViewing("$New_ret", New_Ret);
+
+            PexAssert.IsTrue((New_hsCount == Old_hsCount) && (New_Ret == Old_hsCount));
+        }
+
+        [PexMethod]
+        public void PUT_ContainsContract([PexAssumeUnderTest]HashSet.HashSet<int> hs, int x)
+        {
+            AssumePrecondition.IsTrue(true);
+
+            int Old_hsCount = hs.Count;
+            int Old_x = x;
+            bool Old_Ret = PexChoose.Value<bool>("default_Ret"); ;
+            bool Old_hsContainsX = hs.Contains(x);
+
+            bool New_Ret = hs.Contains(x);
+
+            int New_hsCount = hs.Count;
+            int New_x = x;
+            bool New_hsContainsX = hs.Contains(x);
+
+            PexObserve.ValueForViewing("$old_hs_Count", Old_hsCount);
+            PexObserve.ValueForViewing("$new_hs_Count", New_hsCount);
+            PexObserve.ValueForViewing("$old_hs_x", Old_x);
+            PexObserve.ValueForViewing("$new_hs_x", New_x);
+            PexObserve.ValueForViewing("$old_hs_contains_x", Old_hsContainsX);
+            PexObserve.ValueForViewing("$new_hs_contains_x", New_hsContainsX);
+            PexObserve.ValueForViewing("$Old_ret", Old_Ret);
+            PexObserve.ValueForViewing("$New_ret", New_Ret);
+
+            PexAssert.IsTrue((New_hsCount == Old_hsCount) && (New_hsContainsX == Old_hsContainsX) && (New_x == Old_x));
+        }
+    }
+}
